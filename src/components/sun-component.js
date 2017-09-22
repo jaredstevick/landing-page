bf2fa7cf1cfd8ca6bab62cff26f49114ef426005
Jared Stevick <jstevick@gmail.com>
2017-09-22 12:06:32 -0400

    adding simple animation

---
+++ src/components/sun-component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SunComponent extends React.Component {
+  render() {
+    return (
+      <div className="sun">
+        <div className="rays">
+          <div className="ray ray-1"></div>
+          <div className="ray ray-2"></div>
+          <div className="ray ray-3"></div>
+          <div className="ray ray-4"></div>
+          <div className="ray ray-5"></div>
+          <div className="ray ray-6"></div>
+          <div className="ray ray-7"></div>
+          <div className="ray ray-8"></div>
+          <div className="ray ray-9"></div>
+          <div className="ray ray-10"></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SunComponent;
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import UserForm from '../components/user-form-component';
+import SunComponent from '../components/sun-component';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="page home">
+        <SunComponent />
         <div className="mountains"></div>
         <div className="content">
           <div className="logo"></div>
